03f1cb2d0c2bff21d589ae4028d06e37478af8cf
thangchung <thangchung@ymail.com>
2016-07-15 18:15:39 +0700

    add sample service

---
+++ src/Services/Sample/Cik.Services.Sample.SampleService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Cik.Api.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Cik.Services.Sample.SampleService
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseUrls(
+          new ConfigurationBuilder()
+            .BuildHostConfiguration()
+            .GetUrlForDocker("sample_service_url"))
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
