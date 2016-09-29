a79031ae5cb82b6d1ca2c83aa0afe515dc72e6a3
thangchung <thangchung@ymail.com>
2016-09-29 20:04:20 +0700

    #24 remove un-used files

--- src/UI/backoffice/elm-stuff/packages/lukewestby/elm-http-builder/3.0.0/scripts/unit_tests.sh
+++
@@ -1,4 +0,0 @@
-cd ./tests/unit;
-elm-package install -y
-elm-make ./TestRunner.elm --output ./tests.js;
-node ./tests.js;
--- src/UI/backoffice/elm-stuff/packages/lukewestby/elm-http-builder/3.0.0/tests/unit/TestRunner.elm
+++
@@ -1,9 +0,0 @@
-module Main exposing (..)
-
-import ElmTest exposing (..)
-import Tests
-
-
-main : Program Never
-main =
-    runSuite Tests.all
